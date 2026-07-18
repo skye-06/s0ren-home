4c57ae89ac881d347444dfb148cbddb1c97be908
S0ren <389244295@qq.com>
2026-07-19 02:59:27 +0800

    chore: configure production canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://s0ren-home.edgeone.app/",
+	site: "https://s0ren-home-xk8rcnfv.edgeone.cool/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
